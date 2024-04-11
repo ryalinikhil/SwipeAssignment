b1dd9dfcb7bc9049b289ab7d5098ebf762e52266
ryalinikhil <sameernikhil1234@gmail.com>
2024-04-11 15:58:12 +0530

    Initial Commit

---
+++ SwipeAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwipeAssignment
+//
+//  Created by Sameer Nikhil on 11/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwipeAssignment/SwipeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeAssignmentApp.swift
+//  SwipeAssignment
+//
+//  Created by Sameer Nikhil on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
